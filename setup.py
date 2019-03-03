5d8915feffbd2c9f4bdec642dd3bc735c3587dd6
yutiansut <yutiansut@qq.com>
2019-03-03 21:34:50 +0800

    #update

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ LICENSE = "MIT"
 
 setup(
     name=NAME,
-    version='1.4.0',
+    version='1.4.1',
     description=DESCRIPTION,
     long_description='quantaxis webserver',
     classifiers=[
@@ -42,7 +42,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     install_requires=['quantaxis_run>=1.3',
-                      'quantaxis>=1.3.0.dev6', 'terminado'],
+                      'quantaxis>=1.3.0', 'terminado'],
     entry_points={
         'console_scripts': [
             'quantaxis_webserver=QAWebServer.QA_Web:main',
