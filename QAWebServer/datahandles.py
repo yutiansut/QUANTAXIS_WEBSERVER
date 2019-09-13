348faafcf883dff13ee751c865e3e42706cc02ab
yutiansut <yutiansut@qq.com>
2019-09-13 21:08:26 +0800

    #

--- QAWebServer/datahandles.py
+++ QAWebServer/datahandles.py
@@ -38,7 +38,7 @@ from QAWebServer.fetch_block import get_block, get_name
 from QUANTAXIS.QAFetch.Fetcher import QA_quotation
 from QUANTAXIS.QAFetch.QAQuery import (QA_fetch_stock_day, QA_fetch_stock_min,
                                        QA_fetch_stock_to_market_date)
-from QUANTAXIS.QAFetch.QATdx import QA_fetch_get_future_list, QA_fetch_get_stock_list
+from QUANTAXIS.QAFetch.QATdx import QA_fetch_get_future_list, QA_fetch_get_stock_list, QA_fetch_get_usstock_list, QA_fetch_get_index_list
 from QUANTAXIS.QAFetch.QAQuery_Advance import (QA_fetch_stock_day_adv,
                                                QA_fetch_stock_min_adv)
 from QUANTAXIS.QAUtil.QADate_trade import (QA_util_get_last_day,
@@ -223,8 +223,11 @@ class FutureHandler(QABaseHandler):
 
 class CurrentListHandler(QABaseHandler):
     def get(self):
-        currentlist = pd.concat([QA_fetch_get_stock_list().assign(
-            market='stock_cn'), QA_fetch_get_future_list().assign(market='future_cn')], sort=False)
+        currentlist = pd.concat([
+            QA_fetch_get_stock_list().assign(market='stock_cn'), 
+            QA_fetch_get_index_list().assign(market='index_cn'), 
+            QA_fetch_get_hkstock_list().assign(market='stock_hk'), 
+            QA_fetch_get_future_list().assign(market='future_cn')], sort=False)
         data = (currentlist.code + '/' + currentlist.name + '/' + currentlist.market).tolist()
         self.write({'result': data})
 
