eb2e33270cf16005039e15962389dee43e4a12b3
yutiansut <yutiansut@qq.com>
2020-04-26 17:25:54 +0800

    #

--- QAWebServer/hqcharthandler.py
+++ QAWebServer/hqcharthandler.py
@@ -36,6 +36,9 @@
 a python api for hqchart api
 """
 from QAWebServer.basehandles import QABaseHandler
+import QUANTAXIS as QA
+import pandas as pd
+import datetime
 
 
 class HqTrendSlice():
@@ -53,29 +56,29 @@ class HqTrendSlice():
     @property
     def avgprice(self):
         return 0
+
     @property
     def increase(self):
         return 0
+
     @property
     def risefall(self):
         return 0
 
-
-
     def to_json(self):
         return {
-          'price': self.price,
-          'open': self.open,
-          'high': self.high, 
-          'low': self.low, 
-          'vol': self.vol,
-          'amount': self.amount,
-          'time': self.time,
-          'avgprice': self.avgprice,
-          'increase': self.increase,
-          'risefall': self.risefall,
-          'code': self.code,
-          'close': self.close
+            'price': self.price,
+            'open': self.open,
+            'high': self.high,
+            'low': self.low,
+            'vol': self.vol,
+            'amount': self.amount,
+            'time': self.time,
+            'avgprice': self.avgprice,
+            'increase': self.increase,
+            'risefall': self.risefall,
+            'code': self.code,
+            'close': self.close
 
         }
 
@@ -117,13 +120,50 @@ class HqTrend():
             'minute': self.minute}
 
 
-class QAHqchartHandler(QABaseHandler):
+class HqKline():
+    def __init__(self,code, start, end):
+        self.symbol = code
+
+        self.start = start
+        self.end = self.end
+        
+
+    @property
+    def data(self):
+        return QA.QA_fetch_stock_day(self.symbol[0:6], self.start, self.end, 'pd')
+
+    def klineformat(self):
+        return []
+
+    def to_json(self):
+        return {
+            "data": self.data.assign(date=self.data.date.apply(lambda x: QA.QA_util_date_str2int(str(x)[0:10])), yclose=self.data.close.shift()).loc[:, ['date', 'yclose', 'open', 'high', 'low', 'close', 'volume', 'amount']].values.tolist(),
+            "symbol": self.symbol,  # 股票代码
+            "name": "浦发银行",  # 股票名称
+            "start": 4837,  # 返回数据的起始位置 （暂时不用， 分页下载历史数据使用，下载都是一次请求完)
+            "end": 3838,  # 返回数据的结束位置（暂时不用， 分页下载历史数据使用，下载都是一次请求完)
+            "count": 4838,  # 需要的K线数据个数 ， 单位是天
+            "ticket": 0,
+            "version": "2.0",
+            "message": '',
+            "code": 0,
+            "servertime": str(datetime.date.now())}
+
+
+class QAHqchartDailyHandler(QABaseHandler):
     def get(self):
 
         t = HqTrend()
         t.recv()
         self.write({'result': t.to_json()})
 
+
+class QAHqchartKlineHandler(QABaseHandler):
+    def get(self):
+
+        t = HqKline('600000.sh', '2019-01-01', '2020-01-01')
+        #t#.recv()
+        self.write({'result': t.to_json()})
 
 if __name__ == "__main__":
     import tornado
@@ -132,7 +172,8 @@ if __name__ == "__main__":
 
     app = Application(
         handlers=[
-            (r"/test",  QAHqchartHandler),
+            (r"/test",  QAHqchartDailyHandler),
+            (r"/testk", QAHqchartKlineHandler)
         ],
         debug=True
     )
